ebee4df6801ef56f78bc3fa41462abf8ac66745d
Julio <julio.aparecido3@gmail.com>
2024-05-20 09:00:25 -0300

    json

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+document.getElementById('toggle-dark-mode').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+    const icon = this.querySelector('i');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
     flatpickr("#dataIngresso, #paquisitivoinicio, #paquisitivofim, #dataNascimento, #periodo11, #periodo12, #periodo21, #periodo22, #periodo31, #periodo32", {
         dateFormat: "d/m/Y", // Define o formato da data como DD/MM/AAAA
