736146573ecc614c6953cff5f113c4360803096a
Julio <julio.aparecido3@gmail.com>
2024-05-30 11:26:57 -0300

    json

--- script.js
+++ script.js
@@ -64,7 +64,43 @@ document.addEventListener('DOMContentLoaded', function() {
     updatePeriodVisibility();
 });
 
+function verificapontuacao(matricula) {
+    const usuario = database[matricula];
+    if (!usuario) {
+        alert("Usuário não encontrado.");
+        return false;
+    }
 
+    const pontuacaoAtual = {
+        escolar: usuario.pontuacaoferiasescolar,
+        naoEscolar: usuario.pontuacaoferiasNaoescolar
+    };
+
+    let maiorPontuacaoEscolar = 0;
+    let maiorPontuacaoNaoEscolar = 0;
+
+    for (let matricula in database) {
+        const pontuacao = database[matricula];
+        if (pontuacao.pontuacaoferiasescolar > maiorPontuacaoEscolar) {
+            maiorPontuacaoEscolar = pontuacao.pontuacaoferiasescolar;
+        }
+        if (pontuacao.pontuacaoferiasNaoescolar > maiorPontuacaoNaoEscolar) {
+            maiorPontuacaoNaoEscolar = pontuacao.pontuacaoferiasNaoescolar;
+        }
+    }
+
+    const podeCadastrar = (pontuacaoAtual.escolar >= maiorPontuacaoEscolar) && (pontuacaoAtual.naoEscolar >= maiorPontuacaoNaoEscolar);
+
+    if (!podeCadastrar) {
+        alert("Sua pontuação não é suficiente para o cadastro.");
+    } else {
+        alert("Pontuação suficiente para cadastro.");
+    }
+
+    return podeCadastrar;
+
+    
+}
 
 
 // Simula uma base de dados
