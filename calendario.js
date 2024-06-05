c6b9b433014c7406c0c37f2a7f316dde5a463071
Julio <julio.aparecido3@gmail.com>
2024-06-05 16:35:01 -0300

    json

--- calendario.js
+++ calendario.js
@@ -145,6 +145,15 @@ function criarCalendario(database, ano, containerId, cargos) {
     }
 }
 
-function toggleDarkMode() {
+
+document.getElementById('toggle-dark-mode').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
-}
+    const icon = this.querySelector('i');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+});
